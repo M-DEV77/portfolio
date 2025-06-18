6d7b014becef6389eece6a03de7a750497073453
M-DEV77 <marceloolimpiofbc@gmail.com>
2025-06-18 00:10:40 -0300

    rev3

--- js/main.js
+++ js/main.js
@@ -2,6 +2,41 @@
  const title = document.getElementsByTagName('title')[0];
  const nome = document.getElementById('nome');
  const foto = document.getElementById('foto');
+ const btnCell = document.getElementById('btnCell');
+ const menu = document.getElementById('menu');
+ let scrX = window.innerWidth
+ var nun = 0
+ function numbero(){
+  nun = 0
+ }
+numbero();
+if(scrX > 768){
+  nun = 0
+ }
+addEventListener('click',()=>{
+ 
+   if(nun===0){
+    nun = 1
+   }else{
+    nun = 0
+   }
+
+   
+if(nun===1){
+  menu.style.display="flex";
+  menu.style.height= 20+'vh';
+ 
+}else{
+  menu.style.display="none";
+  
+  
+}   
+
+console.log(scrX)
+
+})
+
+
  fetch('arquivo.txt')
 .then(response => response.text())
 .then(text => {
